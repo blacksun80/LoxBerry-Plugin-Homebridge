fc15a82104d9b5ae542572347de8da4ee730e1c7
blacksun80 <kaufmann-michael@outlook.com>
2023-04-02 20:35:02 +0200

    Update postroot.sh

--- postroot.sh
+++ postroot.sh
@@ -1,16 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-# Bash script which is executed in case of an update (if this plugin is already
-# installed on the system). This script is executed as very first step (*BEFORE*
-# preinstall.sh) and can be used e.g. to save existing configfiles to /tmp 
-# during installation. Use with caution and remember, that all systems may be
-# different!
+# Shell script which is executed by bash *BEFORE* installation is started
+# (*BEFORE* preinstall and *BEFORE* preupdate). Use with caution and remember,
+# that all systems may be different!
 #
 # Exit code must be 0 if executed successfull. 
 # Exit code 1 gives a warning but continues installation.
 # Exit code 2 cancels installation.
 #
-# Will be executed as user "loxberry".
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# Will be executed as user "root".
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #
 # You can use all vars from /etc/environment in this script.
 #
@@ -32,8 +32,9 @@ PTEMPDIR=$1   # First argument is temp folder during install
 PSHNAME=$2    # Second argument is Plugin-Name for scipts etc.
 PDIR=$3       # Third argument is Plugin installation folder
 PVERSION=$4   # Forth argument is Plugin version
-#LBHOMEDIR=$5 # Comes from /etc/environment now. Fifth argument is
+LBHOMEDIR=$5 # Comes from /etc/environment now. Fifth argument is
               # Base folder of LoxBerry
+PTEMPPATH=$6  # Sixth argument is full temp path during install (see also $1)
 
 # Combine them with /etc/environment
 PCGI=$LBPCGI/$PDIR
@@ -45,36 +46,14 @@ PCONFIG=$LBPCONFIG/$PDIR
 PSBIN=$LBPSBIN/$PDIR
 PBIN=$LBPBIN/$PDIR
 
-echo "<INFO> Command is: $COMMAND"
-echo "<INFO> Temporary folder is: $TEMPDIR"
-echo "<INFO> (Short) Name is: $PSHNAME"
-echo "<INFO> Installation folder is: $ARGV3"
-echo "<INFO> Plugin version is: $ARGV4"
-echo "<INFO> Plugin CGI folder is: $PCGI"
-echo "<INFO> Plugin HTML folder is: $PHTML"
-echo "<INFO> Plugin Template folder is: $PTEMPL"
-echo "<INFO> Plugin Data folder is: $PDATA"
-echo "<INFO> Plugin Log folder (on RAMDISK!) is: $PLOG"
-echo "<INFO> Plugin CONFIG folder is: $PCONFIG"
-
-# To use important variables from command line use the following code:
-ARGV0=$0 # Zero argument is shell command
-#echo "<INFO> Command is: $ARGV0"
-
-ARGV1=$1 # First argument is temp folder during install
-#echo "<INFO> Temporary folder is: $ARGV1"
-
-ARGV2=$2 # Second argument is Plugin-Name for scipts etc.
-#echo "<INFO> (Short) Name is: $ARGV2"
-
-ARGV3=$3 # Third argument is Plugin installation folder
-#echo "<INFO> Installation folder is: $ARGV3"
-
-ARGV4=$4 # Forth argument is Plugin version
-#echo "<INFO> Installation folder is: $ARGV4"
-
-ARGV5=$5 # Fifth argument is Base folder of LoxBerry
-#echo "<INFO> Installation folder is: $ARGV5"
+#. $LBHOMEDIR/libs/bashlib/loxberry_log.sh
+#PACKAGE=${PSHNAME}
+#NAME=preroot_install
+#FILENAME=${LBPLOG}/${PSHNAME}/preroot_install.log
+#APPEND=1
+#STDERR=1
+  
+echo "<INFO> Installation as root user started."
 
 # Homebridge / Homebridge Config UI installieren
 npm install -g --unsafe-perm homebridge homebridge-config-ui-x
